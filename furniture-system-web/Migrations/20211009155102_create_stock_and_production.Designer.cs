b1f0f97ff801c4e3f882478e49893fd868340894
supun samarawickrama <supun.samarawickrama@hayleysbsi.com>
2021-10-09 21:31:07 +0530

    migrate1009

--- furniture-system-web/Migrations/20211009155102_create_stock_and_production.Designer.cs
+++ furniture-system-web/Migrations/20211009155102_create_stock_and_production.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using furniture_system_web.Model;
 
 namespace furniture_system_web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class AuthenticationContextModelSnapshot : ModelSnapshot
+    [Migration("20211009155102_create_stock_and_production")]
+    partial class create_stock_and_production
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ furniture-system-web/Migrations/20211009155102_create_stock_and_production.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace furniture_system_web.Migrations
+{
+    public partial class create_stock_and_production : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Retail_Price",
+                table: "products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Retail_Price",
+                table: "products");
+        }
+    }
+}
